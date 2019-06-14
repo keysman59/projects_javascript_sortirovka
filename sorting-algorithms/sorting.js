abae90044ed6e0a7269173ea046363c58c56fede
Ключников Дмитрий <klyuchnikov@ndflka.me>
2019-06-14 14:44:59 +0300

    delete old version

--- sorting-algorithms/sorting.js
+++ sorting-algorithms/sorting.js
@@ -8,9 +8,16 @@ console.log(arr);
 let a = 0;
 let shetchik_zamen = 0;
 for(let h=0; h < arr.length; h++) {
-    let _count = 0;
-    for(let i=0; i < arr.length; i++) {
-        if(arr[i] > arr[i+1]) { //a=5[i] b=2[i-1]
+    for(let i = h + 0; i < arr.length; i++) {
+        if(arr[arr.length-i-1] > arr[arr.length-i]) {
+            a = arr[arr.length-i-1];
+            arr[arr.length-i-1] = arr[arr.length-i];
+            arr[arr.length-i] = a;    
+            shetchik_zamen++;
+        } 
+    } 
+    for(let i= h + 0; i < arr.length; i++) {
+        if(arr[i] > arr[i+1]) {
             a = arr[i];
             arr[i] = arr[i+1];
             arr[i+1] = a;    
@@ -18,15 +25,7 @@ for(let h=0; h < arr.length; h++) {
         } 
         count++;
     }
-    for(let i=0; i < arr.length; i++) {
-        if(arr[i] < arr[i+1]) { //a=5[i] b=2[i-1]
-            a = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = a;    
-            shetchik_zamen++;
-        } 
-        count++;
-    }
+    
     console.log(h+1+" Проход "+arr);
 }
 console.log(arr);
